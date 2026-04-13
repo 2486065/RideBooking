8bd25187f400377ba35d224d3c9e986739745f2d
2486065 <2486065@cognizant.com>
2026-04-13 11:54:51 +0530

    Adding Fall-Back

--- src/main/java/com/cts/feign/AuthServiceClient.java
+++ src/main/java/com/cts/feign/AuthServiceClient.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 
-@FeignClient(name = "AUTH-SERVICE")
+@FeignClient(name = "AUTH-SERVICE", fallbackFactory = AuthServiceClientFallbackFactory.class)
 public interface AuthServiceClient {
 
     @GetMapping("/api/users/{userId}")
